60fb1ace0775c51ea7ee4b1c28aad0610fd3698d
Florin Pop <popflorin1705@yahoo.com>
2021-07-05 10:33:33 +0300

    added products + launch

--- script.js
+++ script.js
@@ -41,89 +41,105 @@ function countdown() {
 
     const startTime = new Date();
     const diff = endTime - startTime;
-    const days = Math.floor(diff / 1000 / 60 / 60 / 24);
-    const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
-    const minutes = Math.floor(diff / 1000 / 60) % 60;
-    const seconds = Math.floor(diff / 1000) % 60;
-    daysEl.innerHTML = days;
-    hoursEl.innerHTML = hours < 10 ? "0" + hours : hours;
-    minEl.innerHTML = minutes < 10 ? "0" + minutes : minutes;
-    secEl.innerHTML = seconds < 10 ? "0" + seconds : seconds;
+
+    if (diff <= 0) {
+        daysEl.innerHTML = 0;
+        hoursEl.innerHTML = "00";
+        minEl.innerHTML = "00";
+        secEl.innerHTML = "00";
+    } else {
+        const days = Math.floor(diff / 1000 / 60 / 60 / 24);
+        const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
+        const minutes = Math.floor(diff / 1000 / 60) % 60;
+        const seconds = Math.floor(diff / 1000) % 60;
+        daysEl.innerHTML = days;
+        hoursEl.innerHTML = hours < 10 ? "0" + hours : hours;
+        minEl.innerHTML = minutes < 10 ? "0" + minutes : minutes;
+        secEl.innerHTML = seconds < 10 ? "0" + seconds : seconds;
+    }
 }
 
 function createFreeProduct(product) {
-    const a = document.createElement("a");
-    a.innerHTML = `
-        <img
-            class="w-full h-full object-cover"
-            src="${product.image_url}"
-            alt="${product.title}"
-        />
-        <span
-            class="
-                text-2xl text-white
-                font-bold
-                absolute
-                bottom-0
-                left-0
-                bg-purple-700
-                py-2
-                px-3
-                rounded-tr-sm
-            "
-        >
-            FREE
-        </span>
+    const wrapper = document.createElement("a");
+    wrapper.innerHTML = `
+        <div class="block w-full h-60 relative">
+            <img
+                class="w-full h-full object-cover"
+                src="${product.image_url}"
+                alt="${product.title}"
+            />
+            <span
+                class="
+                    text-2xl text-white
+                    font-bold
+                    absolute
+                    bottom-0
+                    left-0
+                    bg-purple-700
+                    py-2
+                    px-3
+                    rounded-tr-sm
+                "
+            >
+                FREE
+            </span>
+        </div>
+        <h3 class="text-white text-3xl text-center my-4">${product.title}</h3>
     `;
 
-    a.href = product.url;
-    a.className = `w-full max-w-sm h-60 relative rounded-sm overflow-hidden transform transition hover:-translate-y-1 hover:shadow-xl`;
+    wrapper.href = product.short_url;
+    wrapper.target = "_blank";
+    wrapper.className = `w-full max-w-sm transform transition hover:-translate-y-1 hover:shadow-xl`;
 
-    freeDealsEl.appendChild(a);
+    freeDealsEl.appendChild(wrapper);
 }
 
 function createFeaturedProduct(product) {
-    const a = document.createElement("a");
-    a.innerHTML = `
-        <span
-            class="
-                bg-purple-700
-                text-white text-3xl
-                font-semibold
-                py-3
-                px-4
-                top-0
-                right-0
-                absolute
-                rounded-bl-sm
-            "
-            >Featured</span
-        >
+    const wrapper = document.createElement("a");
+    wrapper.innerHTML = `
+        <div class="block w-full h-96 relative">
+            <span
+                class="
+                    bg-purple-700
+                    text-white text-3xl
+                    font-semibold
+                    py-3
+                    px-4
+                    top-0
+                    right-0
+                    absolute
+                    rounded-bl-sm
+                "
+                >Featured</span
+            >
 
-        <img
-            class="w-full h-full object-cover"
-            src="${product.image_url}"
-            alt="${product.title}"
-        />
-        <span
-            class="
-                text-4xl text-white
-                absolute
-                bottom-0
-                left-0
-                bg-purple-700
-                py-2
-                px-3
-                rounded-tr-sm
-            "
-        >
-            $${product.new_price}
-            <del class="text-lg">$${product.old_price}</del>
-        </span>
+            <img
+                class="w-full h-full object-cover"
+                src="${product.image_url}"
+                alt="${product.title}"
+            />
+            <span
+                class="
+                    text-4xl text-white
+                    absolute
+                    bottom-0
+                    left-0
+                    bg-purple-700
+                    py-2
+                    px-3
+                    rounded-tr-sm
+                "
+            >
+                $${product.new_price}
+                <del class="text-2xl">$${product.old_price}</del>
+            </span>
+        </div>
+        <h3 class="text-white text-3xl text-center my-4">${product.title}</h3>
     `;
 
-    a.href = product.url;
-    a.className = `w-full md:max-w-xl h-96 relative rounded-sm overflow-hidden transform transition hover:-translate-y-1 hover:shadow-xl`;
+    wrapper.href = product.short_url;
+    wrapper.target = "_blank";
+    wrapper.className = `w-full md:max-w-xl transform transition hover:-translate-y-1 hover:shadow-xl`;
 
-    featuredDealsEl.appendChild(a);
+    featuredDealsEl.appendChild(wrapper);
 }
