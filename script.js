3cb0142d02d4c2c1a7793118045ceaee4f863523
Florin Pop <popflorin1705@yahoo.com>
2021-08-23 16:52:05 +0300

    new deals week-8

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ function createFreeProduct(product) {
     wrapper.innerHTML = `
         <div class="block w-full h-60 relative group bg-gray-800">
             <img
-                class="w-auto mx-auto h-full object-fit"
+                class="w-full h-full object-cover"
                 src="${product.image_url}"
                 alt="${product.title}"
             />
